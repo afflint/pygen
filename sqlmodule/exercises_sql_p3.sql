e9abeca9d5fb6068b4c4036c59cb40080556ca46
stefano.montanelli@unimi.it <stefano.montanelli@unimi.it>
2023-10-09 21:59:06 +0200

    added stuff about class of Oct 9

---
+++ sqlmodule/exercises_sql_p3.sql
@@ -0,0 +1,39 @@
+-- estrarre il numero di business di Pizza Hut
+
+
+-- estrarre il numero complessivo di recensioni ricevute da tutti i Pizza Hut
+
+
+-- estrarre il numero medio di stelle di tutti i Pizza Hut
+
+
+-- estrarre il numero complessivo di categorie disponibili
+
+
+-- estrarre il numero complessivo di opinioni cool rispetto alle recensioni di Susan 
+
+
+-- estrarre il numero di categorie usate da almeno un business
+
+
+-- per ogni business, estrarre il numero di recensioni e il numero massimo di stelle ricevute in una review
+
+
+-- per ogni business di categoria "food", estrarre il numero di recensioni ricevute (considerando anche i business privi di recensioni)
+
+
+-- per ogni business di categoria "food", estrarre il numero di recensioni con meno di 3 stelle 
+
+
+-- per ogni categoria, estrarre il numero di business associati 
+
+
+-- estrarre il business con più di 5 servizi offerti 
+
+
+-- estrarre i nomi dei revisori che sono univoci 
+
+
+-- estrarre il nome e la media delle stelle ricevute da business nella categoria food che offrono almeno 5 servizi
+
+
