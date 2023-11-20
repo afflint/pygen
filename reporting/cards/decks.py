c4c3b0b82d4571bff10012c6481351f09b335d65
Alfio Ferrara <alfio.ferrara@unimi.it>
2023-11-20 12:55:25 +0100

    Lezione 2 reporting

--- reporting/cards/decks.py
+++ reporting/cards/decks.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def create_deck(range_limit: int = 11,
                 symbols: list = None,
                 seeds: tuple = None):
@@ -23,3 +26,50 @@ def blackjack_value(card: tuple) -> int:
         return 10
     else:
         return int(x)
+
+
+def blackjack_hand_value(hand: list) -> int:
+    return sum(blackjack_value(card) for card in hand)
+
+
+def serve_hand_shuffle(deck: list) -> list:
+    hand = []
+    np.random.shuffle(deck)
+    for i in range(2):
+        try:
+            card = deck.pop()
+            hand.append(card)
+        except:
+            return None
+    return hand
+
+
+def serve_hand_index(deck: list) -> list:
+    hand = []
+    for x in range(2):
+        try:
+            i = np.random.randint(0, len(deck))
+            card = deck.pop(i)
+            hand.append(card)
+        except:
+            return None
+    return hand
+
+
+def serve_hand_choice(deck: list) -> list:
+    hand = []
+    card_indexes = np.random.choice(range(len(deck)), size=2, replace=False)
+    for i in card_indexes:
+        hand.append(deck[i])
+    return hand
+
+
+def serve_many_hands(deck: list, number_of_hands: int):
+    hands = []
+    for x in range(number_of_hands):
+        hand = serve_hand_index(deck)
+        if hand is None:
+            deck = create_deck()
+            hand = serve_hand_index(deck)
+        hands.append(hand)
+    return hands
